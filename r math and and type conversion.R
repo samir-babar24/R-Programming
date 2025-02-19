987d277e24f2ec06053972bb8c93d03b003f131d
Samir Sudhir Babar. <samir.babar23@pccoepune.org>
2025-02-20 00:07:32 +0530

    Add files via upload

---
+++ r math and and type conversion.R
@@ -0,0 +1,14 @@
+# type conversion in r
+x<-55
+y<-"samir"
+a<-as.character(x)#as.used to change the datatype
+b<-as.integer(y)
+class(a)
+class(b)
+
+# basic maths in r
+abs(-5.5)#used to convert negative into positive
+max(22,34,12)#to calculate maxm nunmber between them{same happens here with min}
+ceiling(2.5)#gives nearest big integer
+floor(2.5)#gives nearest small integer
+sqrt(25)#gives square root of the number
