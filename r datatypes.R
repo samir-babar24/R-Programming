87313c47ade7ac2458e928d564c92eaf47c7f62f
Samir Sudhir Babar. <samir.babar23@pccoepune.org>
2025-02-23 09:49:32 +0530

    Add files via upload

---
+++ r datatypes.R
@@ -0,0 +1,19 @@
+# datatypes in r
+# numeric ;
+x<- 50
+class(x)#this is used to see which type of data it is
+#2
+x<- 50
+class(x)
+# 3
+x<-"z"
+class(x)
+#4
+x<- 7i+3
+class(x) 
+#5
+x<-"samir"
+class(x)
+# 6
+x<-TRUE
+class(x)
