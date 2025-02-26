98a2b798e511d0b25ec487f681f130fa1f36b02e
Samir Sudhir Babar. <samir.babar23@pccoepune.org>
2025-02-26 14:20:47 +0530

    Add files via upload

---
+++ q1.R
@@ -0,0 +1,22 @@
+dataofsociety <- data.frame(
+  
+  Name = c("Omkar Patil", "Samir Babar", "Laxman Paatil",
+           "Piyush Patil", "Vijay Dhame", "Pratik Ghvate", 
+           "Virendra Jagtap", "Trupti Mahadik", "Abhijit Shinde",
+           "Ajim Pathan"),
+  
+  flatno = c(101, 122, 444, 323, 556, 387, 6675, 456, 887, 332),
+  
+  birthdate = c("2/2/1991", "10/7/1990", "11/3/1994", "6/2/1977", 
+                "1/9/1999", "23/8/1991", "30/5/1964", "22/2/1980", 
+                "6/11/1977", "11/2/1991"),
+  
+  bloodgroup = c("O", "A", "B", "AB", "O-", "O+",
+                 "A+", "A-", "B+", "B-"),
+  totalmembers = c(4, 3, 5, 6, 3, 4, 5, 8, 2, 10),
+  statusc = c("rented", "owned", "rented", "owned", "rented", 
+              "owned", "owned", "rented", "owned", "owned")
+)
+
+print(dataofsociety)
+
