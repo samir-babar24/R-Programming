82d46dcbe6e3f51a405413047885fd3bdad0ebb9
Samir Sudhir Babar. <samir.babar23@pccoepune.org>
2025-03-19 06:23:47 +0530

    Add files via upload

---
+++ lists.R
@@ -0,0 +1,13 @@
+#list
+list1<-list("rajesh","trupti","bhairav")
+
+list1
+#access/change inlist
+list1[2]#access
+list1[2]<-"piyusha"# making change
+list1[2]
+length(list1)#calculating the lenght of list
+(list1)[1:2]#only to print specific data in a list
+list2<-list("samir","piyush","harshal")
+list3<-c(list1,list2)#to join two lists
+list3
