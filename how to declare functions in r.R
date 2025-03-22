2d21dd794f4ed817fe0405b1a3101fd8e1e8469b
Samir Sudhir Babar. <samir.babar23@pccoepune.org>
2025-03-22 10:28:21 +0530

    Add files via upload

---
+++ how to declare functions in r.R
@@ -0,0 +1,14 @@
+# declaring function in R
+rfunction <- function(a,b){
+sum <- a+b
+ 
+}
+
+add =rfunction(5,10)
+print(add)
+#heres the syntax
+#funcionname<- function(parameters)
+#{
+ # oprations
+#}
+#declarations  
