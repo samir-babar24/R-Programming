39b969e32e64512f16c4564a9915edbc3bf2f9d7
Samir Sudhir Babar. <samir.babar23@pccoepune.org>
2025-03-26 15:14:07 +0530

    Add files via upload

---
+++ for loop for example.R
@@ -0,0 +1,16 @@
+#for loop for example
+fruits <- list("apple", "banana", "cherry")
+
+for (x in fruits) {
+  print(x)
+}
+#this is called as nested function
+students<-list("samir","piyush","rajesh")
+roll_no<-list(1,2)
+for(x in students){
+  for(y in roll_no){
+    print(x)
+    print(y)
+  }
+}
+
