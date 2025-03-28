a370a7ede0c2d3bbdff25617793fa8111b0fec95
Samir Sudhir Babar. <samir.babar23@pccoepune.org>
2025-03-28 10:57:04 +0530

    Add files via upload

---
+++ factor better than  vector.R
@@ -0,0 +1,6 @@
+#factors
+
+#declaration of factors
+myfriends<-factor(c("piyush","abhi","sarkya","arnav","omkar","kaavathya"))
+myfriends
+#this also have other functions like we have stored in vector and list
