6c91bcf3b0ecd5ca7da17bf7a3e84e8f93b1fed1
Samir Sudhir Babar. <samir.babar23@pccoepune.org>
2025-04-07 06:58:00 +0530

    Add files via upload

---
+++ arrays in r.R
@@ -0,0 +1,8 @@
+#arrays
+#to print array from n to m number
+arr1<-c(1:64)#this will print array from 1 to 55
+arr1
+#we can give shapr to an array using following function
+arr2<-c(arr1,dim=c(4,5,6))
+arr2
+dim(arr2)
